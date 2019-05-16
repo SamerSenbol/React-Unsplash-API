e17eddd0f55549e2ac1026b0da1063551913587b
samer <samer.senbol@gmail.com>
2019-05-17 00:04:44 +0200

    Initail commit.

---
+++ src/components/viewContainer/viewContainer.tsx
@@ -0,0 +1,29 @@
+import React, { Suspense, CSSProperties } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Spinner from '../spinner';
+import searchForm from './searchForm';
+
+const MasterView = React.lazy(() => import(/* webpackChunkName: "masterView" */ './masterView'));
+const DetailView = React.lazy(() => import(/* webpackChunkName: "detailView" */ './detailView/detailView'));
+
+/** React function component */
+export default function ViewContainer() {
+
+    return (
+        <Suspense fallback={<Spinner/>}>
+            <Switch>
+                
+                <Route exact path="/" component={DetailView}/>
+                <div style={centeredForm}>
+                    <searchForm />
+                </div>
+                <Route path="/**" component={DetailView}/>
+            </Switch>
+        </Suspense>
+    );
+}
+const centeredForm: CSSProperties = {
+    height: '100%',
+    textAlign: 'center',
+    marginTop: '5em'
+}
