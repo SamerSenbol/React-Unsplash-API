e70a70404ea97258a45c9f341697d05a82b0654c
SamerSenbol <samer.senbol@gmail.com>
2019-09-25 09:48:50 +0200

    Now You can send the pictuers to local storage when you click in the heart.

--- src/components/viewContainer/detailView/imageSection.tsx
+++ src/components/viewContainer/detailView/imageSection.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Axios, { AxiosResponse } from 'axios';
-import * as ls from "local-storage";
 import ImageCard, { ImageUrls } from './imageCard';
 import { ThemedCSSProperties, ThemeContext } from '../../../contexts/themeContext';
 
@@ -27,6 +26,39 @@ export default class ImageSection extends Component<Props, State> {
         isLiked: true 
     }
 
+    handleLikedImage() {
+        const imageData: ImageUrls[] = JSON.parse(localStorage.getItem(this.props.view) || "{}")
+
+        if(imageData.length > 0) {
+            this.setState ({
+                likedImages: this.state.likedImages = [...imageData]
+            });
+        }
+    }
+
+    LocalStorage() {
+        localStorage.setItem(this.props.view, JSON.stringify(this.state.likedImages))
+    }
+
+    componentDidUpdate() {
+        if(this.props.view in localStorage && this.state.likedImages.length === 0) {
+            this.handleLikedImage();
+
+        } else {
+            this.LocalStorage(); 
+        }
+    }
+
+
+    likedImages = (url: ImageUrls, index: number) => {
+        this.setState({
+            likedImages: [...this.state.likedImages, url]
+        });
+        this.setState({
+            imagesUrls: this.state.imagesUrls.filter((_, i) => i !== index)
+        });
+    }
+
     handleResponse(response: AxiosResponse) {
         if (response.data && response.data.results) {
             const images = response.data.results.map((img: any) => img.urls)
@@ -49,30 +81,6 @@ export default class ImageSection extends Component<Props, State> {
             console.error(error)
         }
     }
-    
-    componentDidUpdate(){
-        if(this.props.view in localStorage && this.state.likedImages.length === 0){
-            const storage: ImageUrls[] = ls.get(this.props.view);
-            if(storage.length > 0) {
-                this.setState({
-                    likedImages: this.state.likedImages = [...storage]
-                })
-            }
-        }else {
-            ls.set(this.props.view, this.state.likedImages);
-        }
-    }
-    
-    handleLikedImage = (urls: ImageUrls, index: number) => {
-        
-        this.setState({
-            likedImages: [...this.state.likedImages, urls]
-        });
-        
-        this.setState({
-            imagesUrls: this.state.imagesUrls.filter((_, i) => i !== index)
-        });
-    }
 
    render() {
         return (
@@ -80,12 +88,12 @@ export default class ImageSection extends Component<Props, State> {
                 {({ theme }) => (
                     <div style={root(theme)}>
                         {this.state.likedImages.map((urls, index) =>
-                            <ImageCard likedClick={this.handleLikedImage} 
+                            <ImageCard likedClick={this.likedImages} 
                             isLiked={true} view= {this.props.view} key={index} urls={urls} 
                             index={index}/>
                         )}
                         {this.state.imagesUrls.map((urls, index) =>
-                            <ImageCard likedClick={this.handleLikedImage} 
+                            <ImageCard likedClick={this.likedImages} 
                             isLiked={false} view= {this.props.view} key={index} urls={urls} 
                             index={index}/>
                         )}
